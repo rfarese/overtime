10f12d54c8e58db7e2c520d4202df317666c62a2
Robert Farese <robertfarese@yahoo.com>
2016-12-16 14:46:34 -0500

    added factorygirl user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryGirl.define do
   factory :user do
-    
+    sequence(:email) { |n| "person#{n}@example.com" }
+    sequence(:first_name) { |n| "John#{n}" }
+    sequence(:last_name) { |n| "Smith#{n}" }
+    password "password"
+    password_confirmation "password"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
   it { should have_valid(:first_name).when("Bobby", "Sarah" )}
   it { should_not have_valid(:first_name).when(nil, "") }
 
@@ -11,11 +12,11 @@ RSpec.describe User, type: :model do
   it { should_not have_valid(:email).when(nil, "", "userMan", "uManDude.com", "@com") }
 
   it 'has a matching password confirmation for the password' do
-    user = User.new
-    user.password = 'password'
-    user.password_confirmation = 'anotherpassword'
+    new_user = User.new
+    new_user.password = 'password'
+    new_user.password_confirmation = 'anotherpassword'
 
-    expect(user).to_not be_valid
-    expect(user.errors[:password_confirmation]).to_not be_blank
+    expect(new_user).to_not be_valid
+    expect(new_user.errors[:password_confirmation]).to_not be_blank
   end
 end
