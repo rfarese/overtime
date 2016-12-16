2d457de3bc92352cb505a465b50a16e4af018cf0
Robert Farese <robertfarese@yahoo.com>
2016-12-16 14:59:52 -0500

    added post resource

---
+++ spec/factories/posts.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :post do
+    date "2016-12-16"
+    rationale "MyText"
+  end
+end
---
+++ spec/models/post_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe Post, type: :model do
+  it { should have_valid(:date).when(Date.today, Date.today - 1.day )}
+  it { should_not have_valid(:date).when(nil, "") }
+
+  it { should have_valid(:rationale).when("This is a rationale", "Something 123" )}
+  it { should_not have_valid(:date).when(nil, "") }
+  
+end
