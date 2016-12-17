4715e1ae5a8ae3293f16bff7b2a5bbba83581f03
Robert Farese <robertfarese@yahoo.com>
2016-12-16 22:46:18 -0500

    removed seeder directory and test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,3 @@
-# require_relative 'seeders/posts_seeder'
-# require_relative 'seeders/user_seeder'
-#
-# # generate all the users
-# users_seeder = UserSeeder.new
-# users_seeder.create_user_attributes(100)
-# users_seeder.seed!
-#
-# # collect all user ids
-# user_ids = User.pluck(:id)
-#
-# # create all the posts
-# posts_seeder = PostsSeeder.new(user_ids)
-# posts_seeder.create_post_attributes(100)
-# posts_seeder.seed!
 @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
 
 puts "1 User created"
