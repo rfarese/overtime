3981854a3c69d4c9cc8bc71cfa88db7da9e85ea7
Robert Farese <robertfarese@yahoo.com>
2016-12-17 14:34:59 -0500

    added notifications with gritter gem

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'uglifier', '>= 1.3.0'
 gem 'jquery-rails'
 gem 'devise'
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.6'
+gem "gritter", "1.2.0"
 
 group :development, :test do
   gem 'capybara'
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,11 @@ class PostsController < ApplicationController
   end
 
   def create
-    post = Post.new(post_params)
-    post.user_id = current_user.id
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id
 
-    if post.save
-      redirect_to post, notice: "Your post was created successfully"
+    if @post.save
+      redirect_to @post, notice: "Your post was created successfully"
     else
       render 'new'
     end
