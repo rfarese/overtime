90fe4a8334b3c248c50b4f60d83f52206a225c96
Robert Farese <robertfarese@yahoo.com>
2016-12-18 10:03:48 -0500

    added enum statuses for post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user
-  
   validates :date, presence: true
   validates :rationale, presence: true
 end
