9670c21261ac1436d59841c545a55b109d5ed465
Robert Farese <robertfarese@yahoo.com>
2016-12-18 10:20:12 -0500

    added status on admin dashboard

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,10 @@
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user = User.create(email: "user@user.com", password: "password", password_confirmation: "password", first_name: "User", last_name: "User")
 
 puts "1 User created"
+
+AdminUser.create(email: "admin@admin.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "User")
+
+puts "1 Admin User created"
 
 100.times do |post|
 	Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
